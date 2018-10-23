3aebe711367dbe41006f88e07c2b1098fd411993
Lingaraj <lingaraj.thangaraja@gmail.com>
2018-10-23 07:17:00 +0000

    Login working with service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
-
+// Fake FakeBackend
+import { fakeBackendProvider } from './_mock/fake-backend';
 //Router
 import { routing } from './app.routing';
 //Guards
@@ -48,7 +49,10 @@ import { SitesComponent } from './sites/sites.component';
     AuthGuard,
     AuthenticationService,
     tokenInterceptor,
-    errorInterceptor
+    errorInterceptor,
+
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
   bootstrap: [AppComponent]
 })
